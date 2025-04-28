83dac0a986206883257fea39752f633f669a9501
Facundofer <101117831+Facundofer@users.noreply.github.com>
2025-04-28 16:08:45 -0300

    Add files via upload

---
+++ programacion2-2025/libs/validaciones/headers/validaciones_TP3_pilas.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+bool esEntero(double valor);
+
+double validar_entero(char *entrada);
+
+Pila cargarpila(int elementos);
+
+void P_intercambiar(Pila pila, Pila auxi);
+
+bool validarejercicio(int valor);
+
+int validareleccion();
+
+bool validarcontinuacion(int valor);
+
+int validarsalida();
+
+int validartamanopila();
+
+int validarentradaentero();
+
+
+void vaciarpila(Pila pila);
